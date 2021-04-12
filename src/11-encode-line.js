3f31cba6552f788b8df75dcc47761702de2ca0af
Serhii <seven8959@gmail.com>
2021-04-12 20:39:55 +0300

    feat: add task 11-encode-line and task 13-stack

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const result = [];
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i + 1] === str[i]) {
+      count++;
+    } else {
+      if (count < 1) {
+        result.push(str[i]);
+      } else {
+        result.push(`${count + 1}${str[i]}`);
+      }
+      count = 0;
+    }
+  }
+
+  return result.join('');
 }
 
 module.exports = encodeLine;
